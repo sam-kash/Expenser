2389278353e5038182b535dec07231673743827b
sam-kash <kashyapsankalpa@gmail.com>
2024-07-03 20:13:34 +0530

    theming attempt

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:expenser/widget/expenses.dart';
 
+var kcolorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
 void main() {
   runApp(
-    const MaterialApp(
-      home: Expenses(),
+    MaterialApp(
+      theme: ThemeData().copyWith(
+          colorScheme: kcolorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+              backgroundColor: kcolorScheme.onPrimaryContainer,
+              foregroundColor: kcolorScheme.primaryContainer)),
+      home: const Expenses(),
     ),
   );
 }
